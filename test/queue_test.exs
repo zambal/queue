d39fc6ebcabddd367524d4e737bb6cae6e112bf4
Vincent Siliakus <zambal@gmail.com>
2015-10-12 19:29:23 +0000

    Added Enumerable and Collectable tests

--- test/queue_test.exs
+++ test/queue_test.exs
@@ -1,6 +1,22 @@
 defmodule QueueTest do
   use ExUnit.Case
 
+  defp push_1_4 do
+    Queue.new()
+    |> Queue.push(1)
+    |> Queue.push(2)
+    |> Queue.push(3)
+    |> Queue.push(4)
+  end
+
+  defp pushr_1_4 do
+    Queue.new()
+    |> Queue.pushr(1)
+    |> Queue.pushr(2)
+    |> Queue.pushr(3)
+    |> Queue.pushr(4)
+  end
+
   test "new queue" do
     assert Queue.new() == %Queue{front: [], rear: []}
   end
@@ -16,29 +32,17 @@ defmodule QueueTest do
   end
 
   test "push and pop from queue" do
-    q = Queue.new()
-    |> Queue.push(1)
-    |> Queue.push(2)
-    |> Queue.push(3)
-    |> Queue.push(4)
+    q = push_1_4
     assert Queue.pop(q) == { 4, %Queue{front: [3, 2], rear: [1]} }
   end
 
   test "push and popr from queue" do
-    q = Queue.new()
-    |> Queue.push(1)
-    |> Queue.push(2)
-    |> Queue.push(3)
-    |> Queue.push(4)
+    q = push_1_4
     assert Queue.popr(q) == { 1, %Queue{front: [4, 3], rear: [2]} }
   end
 
   test "pushr and popr from queue" do
-    q = Queue.new()
-    |> Queue.pushr(1)
-    |> Queue.pushr(2)
-    |> Queue.pushr(3)
-    |> Queue.pushr(4)
+    q = pushr_1_4
     assert Queue.popr(q) == { 4, %Queue{front: [1], rear: [3, 2]} }
   end
 
@@ -48,16 +52,12 @@ defmodule QueueTest do
   end
 
   test "peek queue" do
-    q = Queue.new()
-    |> Queue.push(1)
-    |> Queue.push(2)
-    assert Queue.peek(q) == { :ok, 2 }
+    q = push_1_4
+    assert Queue.peek(q) == { :ok, 4 }
   end
 
   test "peekr queue" do
-    q = Queue.new()
-    |> Queue.push(1)
-    |> Queue.push(2)
+    q = push_1_4
     assert Queue.peekr(q) == { :ok, 1 }
   end
 
@@ -67,20 +67,29 @@ defmodule QueueTest do
   end
 
   test "push and drop from queue" do
-    q = Queue.new()
-    |> Queue.push(1)
-    |> Queue.push(2)
-    |> Queue.push(3)
-    |> Queue.push(4)
+    q = push_1_4
     assert Queue.drop(q) == %Queue{front: [3, 2], rear: [1]}
   end
 
   test "push and dropr from queue" do
-    q = Queue.new()
-    |> Queue.push(1)
-    |> Queue.push(2)
-    |> Queue.push(3)
-    |> Queue.push(4)
+    q = push_1_4
     assert Queue.dropr(q) == %Queue{front: [4, 3], rear: [2]}
+  end
+
+  test "Enumerable implementation" do
+    q = push_1_4
+
+    assert Enum.map(q, &(&1)) == [4, 3, 2, 1]
+    assert Enum.member?(q, 3) == true
+    assert Enum.member?(q, 0) == false
+    assert Enum.count(q) == 4
+    assert Enum.take_while(q, &(&1 > 2)) == [4, 3]
+  end
+
+  test "Collectable implementation" do
+    q1 = push_1_4
+    q2 = for n <- 1..4, into: Queue.new(), do: n
+
+    q1 == q2
   end
 end
